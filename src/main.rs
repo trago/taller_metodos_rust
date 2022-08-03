d2540a2c31bd1938f9fd73cd45ddc1405c135910
Julio C. Estrada <julio@functionize.com>
2022-08-02 22:53:23 -0500

    printing emojis

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 
     {
         let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
+        println!("The value of x in the inner scope is: {x} \u{1F378}");
     }
 
     println!("The value of x is: {x}");
