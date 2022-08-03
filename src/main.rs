e59370ff65f06e2b851f830bbf4c2e6123006843
Julio C. Estrada <julio@functionize.com>
2022-08-02 23:01:00 -0500

    accessing arrays

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use std::io;
+
 fn main() {
-    let tup = (500, 6.4, 1);
+    let a = [1, 2, 3, 4, 5];
 
-    let (x, y, z) = tup;
+    println!("Please enter an array index.");
 
-    println!("The value of y is: {y}");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
