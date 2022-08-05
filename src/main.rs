2de169dbe128fd54f19ac21bd2b13d82f8d67283
Julio C. Estrada <julio@functionize.com>
2022-08-05 06:17:16 -0500

    Statistics

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
-fn main() {
-    let complex_num1 = num::complex::Complex::new(10.0, 20.0); // Must use floats
-    let complex_num2 = num::complex::Complex::new(3.1, -4.2);
+fn mean(data: &[i32]) -> Option<f32> {
+    let sum = data.iter().sum::<i32>() as f32;
+    let count = data.len();
 
-    let sum = complex_num1 + complex_num2;
-
-    println!("Sum: {}", sum);
+    match count {
+        positive if positive > 0 => Some(sum / count as f32),
+        _ => None,
+    }
 }
 
-// use std::f64::consts::PI;
-// use num::complex::Complex;
-//
-// fn main() {
-//     let x = Complex::new(0.0, 2.0*PI);
-//
-//     println!("e^(2i * pi) = {}", x.exp()); // =~1
-// }
+fn std_deviation(data: &[i32]) -> Option<f32> {
+    match (mean(data), data.len()) {
+        (Some(data_mean), count) if count > 0 => {
+            let variance = data.iter().map(|value| {
+                let diff = data_mean - (*value as f32);
+
+                diff * diff
+            }).sum::<f32>() / count as f32;
+
+            Some(variance.sqrt())
+        },
+        _ => None
+    }
+}
+
+fn main() {
+    let data = [3, 1, 6, 1, 5, 8, 1, 8, 10, 11];
+
+    let data_mean = mean(&data);
+    println!("Mean is {:?}", data_mean);
+
+    let data_std_deviation = std_deviation(&data);
+    println!("Standard deviation is {:?}", data_std_deviation);
+
+    let zscore = match (data_mean, data_std_deviation) {
+        (Some(mean), Some(std_deviation)) => {
+            let diff = data[4] as f32 - mean;
+
+            Some(diff / std_deviation)
+        },
+        _ => None
+    };
+    println!("Z-score of data at index 4 (with value {}) is {:?}", data[4], zscore);
+}
