9d9d9d47d7e9fa8da7ebda20297f6ff5962d15ba
Julio C. Estrada <julio@functionize.com>
2022-08-05 05:41:53 -0500

    Multiply a with a vector with a matrix

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use ndarray::arr2;
+use ndarray::{arr1, arr2, Array1};
 
 fn main() {
-    let a = arr2(&[[1, 2, 3],
-        [4, 5, 6]]);
+    let scalar = 4;
 
-    let b = arr2(&[[6, 5, 4],
-        [3, 2, 1]]);
+    let vector = arr1(&[1, 2, 3]);
 
-    let sum = &a + &b;
+    let matrix = arr2(&[[4, 5, 6],
+        [7, 8, 9]]);
 
-    println!("{}", a);
-    println!("+");
-    println!("{}", b);
-    println!("=");
-    println!("{}", sum);
+    let new_vector: Array1<_> = scalar * vector;
+    println!("{}", new_vector);
+
+    let new_matrix = matrix.dot(&new_vector);
+    println!("{}", new_matrix);
 }
