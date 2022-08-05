f0d6f15b949a888b65ab4df13c6ab3f059150736
Julio C. Estrada <julio@functionize.com>
2022-08-05 06:06:10 -0500

    matrix inverse

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-use ndarray::{array, Array1, ArrayView1};
-
-fn l1_norm(x: ArrayView1<f64>) -> f64 {
-    x.fold(0., |acc, elem| acc + elem.abs())
-}
-
-fn l2_norm(x: ArrayView1<f64>) -> f64 {
-    x.dot(&x).sqrt()
-}
-
-fn normalize(mut x: Array1<f64>) -> Array1<f64> {
-    let norm = l2_norm(x.view());
-    x.mapv_inplace(|e| e/norm);
-    x
-}
+use nalgebra::Matrix3;
 
 fn main() {
-    let x: Array1<f64> = array![1., 2., 3., 4., 5.];
-
-    println!("||x||_2 = {}", l2_norm(x.view()));
-    println!("||x||_1 = {}", l1_norm(x.view()));
-    println!("Normalizing x yields {:?}", normalize(x));
+    let m1 = Matrix3::new(2.0, 1.0, 1.0, 3.0, 2.0, 1.0, 2.0, 1.0, 2.0);
+    println!("m1 = {}", m1);
+    match m1.try_inverse() {
+        Some(inv) => {
+            println!("The inverse of m1 is: {}", inv);
+        }
+        None => {
+            println!("m1 is not invertible!");
+        }
+    }
 }
