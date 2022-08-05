60181e39d48841d83f1070402b399f03bf6e574f
Julio C. Estrada <julio@functionize.com>
2022-08-05 06:08:33 -0500

    trigonometric functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use nalgebra::Matrix3;
-
 fn main() {
-    let m1 = Matrix3::new(2.0, 1.0, 1.0, 3.0, 2.0, 1.0, 2.0, 1.0, 2.0);
-    println!("m1 = {}", m1);
-    match m1.try_inverse() {
-        Some(inv) => {
-            println!("The inverse of m1 is: {}", inv);
-        }
-        None => {
-            println!("m1 is not invertible!");
-        }
-    }
+    let angle: f64 = 2.0;
+    let side_length = 80.0;
+
+    let hypotenuse = side_length / angle.sin();
+
+    println!("Hypotenuse: {}", hypotenuse);
 }
+
+// fn main() {
+//     let x: f64 = 6.0;
+//
+//     let a = x.tan();
+//     let b = x.sin() / x.cos();
+//
+//     assert_eq!(a, b);
+// }
