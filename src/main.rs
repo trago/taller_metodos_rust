4bc86e98fe08e2d98ecfeaad3d8617832ff24784
Julio C. Estrada <julio@functionize.com>
2022-08-05 05:58:17 -0500

    vector norm

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use approx::assert_abs_diff_eq;
-use ndarray::Array;
+use ndarray::{array, Array1, ArrayView1};
+
+fn l1_norm(x: ArrayView1<f64>) -> f64 {
+    x.fold(0., |acc, elem| acc + elem.abs())
+}
+
+fn l2_norm(x: ArrayView1<f64>) -> f64 {
+    x.dot(&x).sqrt()
+}
+
+fn normalize(mut x: Array1<f64>) -> Array1<f64> {
+    let norm = l2_norm(x.view());
+    x.mapv_inplace(|e| e/norm);
+    x
+}
 
 fn main() {
-    let a = Array::from(vec![1., 2., 3., 4., 5.]);
-    let b = Array::from(vec![5., 4., 3., 2., 1.]);
-    let mut c = Array::from(vec![1., 2., 3., 4., 5.]);
-    let mut d = Array::from(vec![5., 4., 3., 2., 1.]);
+    let x: Array1<f64> = array![1., 2., 3., 4., 5.];
 
-    let z = a + b;
-    let w =  &c + &d;
-
-    assert_abs_diff_eq!(z, Array::from(vec![6., 6., 6., 6., 6.]));
-
-    println!("c = {}", c);
-    c[0] = 10.;
-    d[1] = 10.;
-
-    assert_abs_diff_eq!(w, Array::from(vec![6., 6., 6., 6., 6.]));
-
+    println!("||x||_2 = {}", l2_norm(x.view()));
+    println!("||x||_1 = {}", l1_norm(x.view()));
+    println!("Normalizing x yields {:?}", normalize(x));
 }
