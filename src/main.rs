858d2f3e2e6f971c8874bbf5b8799905e4fe37d2
Julio C. Estrada <julio@functionize.com>
2022-08-05 06:13:33 -0500

    complex numbers

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-// Distancia entre  dos puntos hacia la tierra usando Haversine formula.
 fn main() {
-    let earth_radius_kilometer = 6371.0_f64;
-    let (paris_latitude_degrees, paris_longitude_degrees) = (48.85341_f64, -2.34880_f64);
-    let (london_latitude_degrees, london_longitude_degrees) = (51.50853_f64, -0.12574_f64);
+    let complex_num1 = num::complex::Complex::new(10.0, 20.0); // Must use floats
+    let complex_num2 = num::complex::Complex::new(3.1, -4.2);
 
-    let paris_latitude = paris_latitude_degrees.to_radians();
-    let london_latitude = london_latitude_degrees.to_radians();
+    let sum = complex_num1 + complex_num2;
 
-    let delta_latitude = (paris_latitude_degrees - london_latitude_degrees).to_radians();
-    let delta_longitude = (paris_longitude_degrees - london_longitude_degrees).to_radians();
-
-    let central_angle_inner = (delta_latitude / 2.0).sin().powi(2)
-        + paris_latitude.cos() * london_latitude.cos() * (delta_longitude / 2.0).sin().powi(2);
-    let central_angle = 2.0 * central_angle_inner.sqrt().asin();
-
-    let distance = earth_radius_kilometer * central_angle;
-
-    println!(
-        "Distance between Paris and London on the surface of Earth is {:.1} kilometers",
-        distance
-    );
+    println!("Sum: {}", sum);
 }
+
+// use std::f64::consts::PI;
+// use num::complex::Complex;
+//
+// fn main() {
+//     let x = Complex::new(0.0, 2.0*PI);
+//
+//     println!("e^(2i * pi) = {}", x.exp()); // =~1
+// }
