3e20e61c72c96bd78b5ea8cda11c66cf0594d1fe
Julio C. Estrada <julio@functionize.com>
2022-08-05 06:22:47 -0500

    Optiom enum

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,36 @@
-fn mean(data: &[i32]) -> Option<f32> {
-    let sum = data.iter().sum::<i32>() as f32;
-    let count = data.len();
-
-    match count {
-        positive if positive > 0 => Some(sum / count as f32),
-        _ => None,
-    }
-}
-
-fn std_deviation(data: &[i32]) -> Option<f32> {
-    match (mean(data), data.len()) {
-        (Some(data_mean), count) if count > 0 => {
-            let variance = data.iter().map(|value| {
-                let diff = data_mean - (*value as f32);
-
-                diff * diff
-            }).sum::<f32>() / count as f32;
-
-            Some(variance.sqrt())
-        },
-        _ => None
-    }
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
 fn main() {
-    let data = [3, 1, 6, 1, 5, 8, 1, 8, 10, 11];
-
-    let data_mean = mean(&data);
-    println!("Mean is {:?}", data_mean);
-
-    let data_std_deviation = std_deviation(&data);
-    println!("Standard deviation is {:?}", data_std_deviation);
-
-    let zscore = match (data_mean, data_std_deviation) {
-        (Some(mean), Some(std_deviation)) => {
-            let diff = data[4] as f32 - mean;
-
-            Some(diff / std_deviation)
-        },
-        _ => None
-    };
-    println!("Z-score of data at index 4 (with value {}) is {:?}", data[4], zscore);
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 }
+
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+//
+// fn main(){
+//     let home = IpAddr::V4(String::from("127.0.0.1"));
+//     let loopback = IpAddr::V6(String::from("::1"));
+// }
+
+// # Option enum
+// enum Option<T> {
+//     None,
+//     Some(T),
+// }
+// fn main(){
+//     let some_number = Some(5);
+//     let some_string = Some("a string");
+//
+//     let absent_number: Option<i32> = None;
+//
+//     match some_number {
+//         Some(v) => println!("The value is {}",v),
+//         None => println!("We don't have a valid value")
+//     }
+// }
